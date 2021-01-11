f97554845f0c8c76f54e0fb16cd30291a724d7ab
Andy Lin <alin21@stuy.edu>
2021-01-11 13:21:42 -0500

    added length

--- Radix.java
+++ Radix.java
@@ -2,7 +2,12 @@ import java.lang.Math;
 
 class Radix{
   public static int nth(int n, int col){
-    return (n/(int)(Math.pow(10, col)))%(int)(Math.pow(10, col));
+    return Math.abs((n/(int)(Math.pow(10, col)))%(int)(Math.pow(10, col)));
   }
-  
+  public static int length(int n){
+    return (int)(Math.log10(Math.abs(n))+1);
+  }
+  public static void main(String[] args) {
+    System.out.println(length(-123));
+  }
 }
